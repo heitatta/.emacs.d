dffcd40887e732d947afb377ebd29476cc840057
Tadashi Hirata <t1-hirata@nri.co.jp>
2018-02-26 10:06:25 +0900

    Fix typo

--- init.el
+++ init.el
@@ -120,7 +120,7 @@
        (setq shell-file-name (executable-find "zsh"))
        (modify-coding-system-alist 'process ".*sh" 'utf-8)))
 (cond ((featurep 'macos)
-       (setq system-uses-terminfo nil))
+       (setq system-uses-terminfo nil)))
 
 (setq shell-edit-mode-map (make-sparse-keymap))
 (defun toggle-shell-edit-mode ()
